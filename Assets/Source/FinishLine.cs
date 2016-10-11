35139e864d59e2a6356c7f2ea5b0f3e211f63bba
Megawats777 <jedsemilla@gmail.com>
2016-10-11 19:40:22 -0400

    Fixed complile error by removing UnityEngine namespace

--- Assets/Source/FinishLine.cs
+++ Assets/Source/FinishLine.cs
@@ -39,15 +39,6 @@ public class FinishLine : MonoBehaviour
     // Draw editor gizmos
     public void OnDrawGizmos()
     {
-        /*--Draw a text label over the object--*/
-
-        // The position of the text label
-        Vector3 textLabelPos = transform.position + new Vector3(0.0f, 1.0f, 0.0f);
-
-        // Set the colour of the text label
-        UnityEditor.Handles.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
-
-        // Set the text of the text label
-        UnityEditor.Handles.Label(textLabelPos, "Finish Line");
+        
     }
 }
